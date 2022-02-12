33df987748359b800b242a0b4b749252eb3a9772
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-02-11 21:19:02 -0300

    changed BASE_URL

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://my-wallet-vitor.herokuapp.com";
 
 function login(clientLogin) {
   return axios.post(`${BASE_URL}/sign-in`, clientLogin);
